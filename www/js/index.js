6efbe70540fadf8a16e56994f9dc5d46f2ad0663
themaxtech <maxtechapps@gmail.com>
2016-02-10 11:23:27 +0530

    po
    po

--- www/js/index.js
+++ www/js/index.js
@@ -117,7 +117,7 @@ var app = {
  
             case 'message':
               // this is the actual push notification. its format depends on the data model from the push server
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+              alert('message = '+e.message+' msgcnt = '+e.price);
 
 
             break;
